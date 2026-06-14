89ed199ce9836fb4bb491d299564738e037b0409
danu1999 <danusijon@gmail.com>
2026-06-14 19:18:59 +0700

    release: POSBah Version 2.0.9 release with automated demo lifecycle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.posbah.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "2.0.8"
+        versionCode = 10
+        versionName = "2.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
