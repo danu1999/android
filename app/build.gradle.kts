b582c86e3313141204ed1e4af0dc8176489dbe42
danu1999 <danusijon@gmail.com>
2026-06-21 01:45:14 +0700

    chore: bump version to v2.17.28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.posbah.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 66
-        versionName = "2.17.27"
+        versionCode = 67
+        versionName = "2.17.28"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
